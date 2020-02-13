0bae2e61b6bafc8f1955120c40dd2b2ee7cf4d3c
Jorge Rios <jorgearios31@gmail.com>
2020-02-13 19:24:37 -0300

    Cambios

---
+++ EjAbstractas/src/Figura.java
@@ -0,0 +1,13 @@
+
+public abstract class Figura {
+	
+	protected double x;
+	protected double y;
+	
+	public Figura(double x, double y) {
+		
+		this.x=x;
+		this.y=y;
+	}
+	public abstract double area();
+}
